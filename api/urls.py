994297f85a33b4ede169cdb754b273b2d022558f
loharaws <127829795+loharaws@users.noreply.github.com>
2026-05-21 20:04:43 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Django Running Successfully")
 
 urlpatterns = [
-    path('account/', include('Account.urls')),
-    path('blog/', include('blogapp.urls')),
+    path('', home),
+    path('admin/', admin.site.urls),
+    path('api/', include('blogapp.urls')),
 ]
-
-
-
